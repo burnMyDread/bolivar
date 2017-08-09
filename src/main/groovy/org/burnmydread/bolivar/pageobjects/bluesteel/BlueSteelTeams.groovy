38141d5d08950057d51465a27c0230991503f6ac
BurnMyDread <zach@valvatorez>
2017-08-09 17:53:17 -0400

    removing the logo from the team page

--- src/main/groovy/org/burnmydread/bolivar/pageobjects/bluesteel/BlueSteelTeams.groovy
+++ src/main/groovy/org/burnmydread/bolivar/pageobjects/bluesteel/BlueSteelTeams.groovy
@@ -7,9 +7,9 @@ import geb.Page
  */
 class BlueSteelTeams extends Page {
     static at = {
-        waitFor(60, 0.25){ cloud_bees_logo.present }
-        waitFor(10, 0.1){ create_team_button.present }
-        waitFor(10, 0.1){ team_search.present }
+        //waitFor(60, 0.25){ cloud_bees_logo.present }
+        waitFor(60, 0.25){ create_team_button.present }
+        waitFor(10, 0.01){ team_search.present }
     }
 
     static content = {
